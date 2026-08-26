a05ebdb0d8c19646934d788c3aca8113fc45b161
KaioBittencourt07 <kaiopdz@gmail.com>
2026-08-26 17:36:08 -0300

    Export historical dataset utilities

--- backtest/src/index.js
+++ backtest/src/index.js
@@ -2,3 +2,4 @@ export { calculateMetrics } from './metrics.js';
 export { groupBy, analyzeSegments } from './grouping.js';
 export { timeSplit } from './split.js';
 export { replay } from './replay.js';
+export { normalizeHistorical, validateHistorical } from './dataset.js';
