3d4a48d17e8e3a7145c699cc8ff6e5e61108fdc4
KaioBittencourt07 <kaiopdz@gmail.com>
2026-08-26 18:01:04 -0300

    Export Twelve Data market provider

--- data/src/index.js
+++ data/src/index.js
@@ -2,3 +2,4 @@ export { createMarketSnapshot, validateSnapshot } from './marketData.js';
 export { normalizeMarketSnapshot, DATA_STATUS } from './marketAdapter.js';
 export { createMarketProvider, createManualProvider } from './provider.js';
 export { createHttpMarketProvider } from './providers/httpProvider.js';
+export { createTwelveDataProvider } from './providers/twelveDataProvider.js';
