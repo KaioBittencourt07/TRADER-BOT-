fca5964b1182fcfe9e0427040f06c3a96b1f6241
KaioBittencourt07 <kaiopdz@gmail.com>
2026-08-26 17:35:56 -0300

    Integrate backtest replay and dataset split

--- backtest/src/index.js
+++ backtest/src/index.js
@@ -1,2 +1,4 @@
 export { calculateMetrics } from './metrics.js';
 export { groupBy, analyzeSegments } from './grouping.js';
+export { timeSplit } from './split.js';
+export { replay } from './replay.js';
